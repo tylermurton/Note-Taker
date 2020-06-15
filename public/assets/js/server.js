62c3a5cf572056bcfc2457bedc8090b578e65b01
Tyler Murton <tylermurton@gmail.com>
2020-06-15 18:08:46 -0400

    updated server

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -6,12 +6,72 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+let notesData = [];
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../../../public")));
+
+app.get("/api/notes", function(err, res) {
+    try {
+        notesData = fs.readFileSync("../../../db/db.json", "utf8");
+        notesData = JSON.parse(notesData);
+    } catch (err) {
+        console.log("\n error (in app.get.catch):");
+        console.log(err);
+    }
+    // send objects to the browswer
+    res.json(notesData);
+});
+
+//write the new note to the json file
 app.post("/api/notes", function(req, res) {
-    /////////////
+    try {
+        notesData = fs.readFileSync("../../../db/db.json", "utf8");
+        console.log(notesData);
+
+        //parse data to get array of objects
+        notesData = JSON.parse(notesData);
+        //set new note's id
+        req.body.id = notesData.length;
+        //add new note to array of note objects
+        notesData.push(req.body);
+        //stringify so you can write it to file
+        notesData = JSON.stringify(notesData);
+        //writes new note to file
+        fs.writeFile("../../../db/db.json", notesData, "utf8", function(err) {
+            if (err) throw err;
+        });
+        // revert back to array of objects & send back to browser
+        res.send(JSON.parse(notesData));
+    } catch (err) {
+        throw err;
+        console.log(err);
+    }
 });
 
 app.delete("/api/notes/:id", function(req, res) {
-    //receive query parameter containing id of note to delete......
+    try {
+        notesData = fs.readFileSync("../../../db/db.json", "utf8");
+
+        //parse data to get array of objects
+        notesData = JSON.parse(notesData);
+        //delete old note from array
+        notesData = notesData.filter(function(note) {
+            return note.id != req.params.id;
+        });
+        //stringify so you can write it to file
+        notesData = JSON.stringify(notesData);
+        //writes new note to file
+        fs.writeFile("../../../db/db.json", notesData, "utf8", function(err) {
+            if (err) throw err;
+        });
+        // revert back to array of objects & send back to browser
+        res.send(JSON.parse(notesData));
+    } catch (err) {
+        throw err;
+        console.log(err);
+    }
 });
 
 // HTML GET REQUESTS
@@ -24,7 +84,7 @@ app.get("*", function(req, res) {
 });
 
 app.get("/api/notes", function(req, res) {
-    return res.sendFile(path.json(__dirname, "db/db.json"));
+    return res.sendFile(path.json(__dirname, "../../../db/db.json"));
 });
 
 
